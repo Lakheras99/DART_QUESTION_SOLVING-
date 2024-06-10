6b1da1896381505ff1567214d731cd93dce91775
Sumer Lakhera <lakheras99@gmail.com>
2024-06-10 18:46:22 +0530

    second program

---
+++ lib/array/middleElementArray.dart
@@ -0,0 +1,7 @@
+// Create an array of integers and print the middle element except first and last.
+
+void main() {
+  List<String> name = ["Sumer", "Thomson", "John", "Andrew", "Lakhera"];
+
+  print(name.getRange(1, 4).join(" "));
+}
