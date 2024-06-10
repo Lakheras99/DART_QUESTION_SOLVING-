72ae320ea64aa34322bc4f63b9e0bfea3d5f7f8f
Sumer Lakhera <lakheras99@gmail.com>
2024-06-10 18:40:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+// Create an array of integers and print the first and last elements.
+
+void main() {
+  List<String> name = ["Sumer", "Thomson", "John", "Andrew", "Lakhera"];
+  print("${name.first} ${name.last}");
+}
